62aeb08a1ea9f0983151e561251d4b07b8522f87
Andrea del Mar Silva Espinel <jsmoraless@udistrital.edu.co>
2026-08-30 16:32:38 -0500

    Método que verifica si un número es primo y Método que genera una serie de Fibonacci

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -46,17 +46,35 @@ public class Workshop {
 
     // Método que verifica si un número es primo
     public boolean esPrimo(int numero) {
-        // TODO: Implementar el método para verificar si un número es primo.
-        // Ejemplo: Si numero = 7, el resultado debería ser true.
+        if (numero <= 1) {
         return false;
+    }
+    
+    for (int i = 2; i < numero; i++) {
+        if (numero % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
     }
 
     // Método que genera una serie de Fibonacci
     public int[] serieFibonacci(int n) {
-        // TODO: Implementar el método para generar la serie de Fibonacci hasta el número n.
-        // Ejemplo: Si n = 5, el resultado debería ser [0, 1, 1, 2, 3].
-        // Lanzar IllegalArgumentException si n es negativo.
-        return new int[0];
+        if (n < 0) {
+        throw new IllegalArgumentException("El número n no puede ser negativo");
+    }
+    
+    int[] F = new int[n];
+    
+    if (n > 0) F[0] = 0;
+    if (n > 1) F[1] = 1;
+    
+    for (int i = 2; i < n; i++) {
+        F[i] = F[i - 1] + F[i - 2];
+    }
+    
+    return F;
     }
 
     // Método que suma todos los elementos de un arreglo
